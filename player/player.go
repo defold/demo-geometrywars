65649146a302352cf8bdec50eeb5d081ac36b1fc
Ragnar Svensson <ragnar.svensson@defold.se>
2012-10-10 19:14:37 +0200

    Player avoidance and random targetting behavior.

--- player/player.go
+++ player/player.go
@@ -14,9 +14,25 @@ components {
   }
 }
 embedded_components {
-  id: "collisionobject"
+  id: "body"
   type: "collisionobject"
   data: "collision_shape: \"\"\ntype: COLLISION_OBJECT_TYPE_KINEMATIC\nmass: 0.0\nfriction: 0.1\nrestitution: 0.5\ngroup: \"player\"\nmask: \"enemy\"\nmask: \"level\"\nembedded_collision_shape {\n  shapes {\n    shape_type: TYPE_BOX\n    position {\n      x: 0.0\n      y: 0.40811595\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n      w: 1.0\n    }\n    index: 0\n    count: 3\n  }\n  data: 15.101449\n  data: 10.918261\n  data: 10.0\n}\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sensor"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\ntype: COLLISION_OBJECT_TYPE_TRIGGER\nmass: 0.0\nfriction: 0.1\nrestitution: 0.5\ngroup: \"player\"\nmask: \"enemy\"\nembedded_collision_shape {\n  shapes {\n    shape_type: TYPE_SPHERE\n    position {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n      w: 1.0\n    }\n    index: 0\n    count: 1\n  }\n  data: 175.19794\n}\n"
   position {
     x: 0.0
     y: 0.0
