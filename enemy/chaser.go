3f5018b2453e0decb6273becd9e312b63d5da267
Christian Murray <cgmurray@gmail.com>
2012-10-10 22:54:19 +0200

    Particle lighting

--- enemy/chaser.go
+++ enemy/chaser.go
@@ -14,6 +14,21 @@ components {
   }
 }
 components {
+  id: "particlefx_light"
+  component: "/enemy/light.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
   id: "script"
   component: "/enemy/chaser.script"
   position {
--- materials/pass.fp
+++ materials/pass.fp
@@ -9,5 +9,4 @@ void main()
     gl_FragColor = texture2D(DIFFUSE_TEXTURE, var_texcoord0.xy);
     float amb = 0.05;
     gl_FragColor.xyz = clamp(gl_FragColor.xyz + vec3(amb, amb, amb), vec3(0, 0, 0), vec3(1, 1, 1));  
-    //gl_FragColor.w = 1.0;
 }
